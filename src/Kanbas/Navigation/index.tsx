4e7d05066a719356cea0ed9a47870345ecde92cd
Yiwei Wang <ivwang@yahoo.com>
2024-02-25 16:54:22 -0500

    updated

---
+++ src/Kanbas/Navigation/index.tsx
@@ -0,0 +1,36 @@
+import { Link, useLocation } from "react-router-dom";
+import "./index.css";
+import { FaTachometerAlt, FaRegUserCircle, FaBook, FaRegCalendarAlt,  FaInbox, FaComment } from "react-icons/fa";
+import { CiClock2 } from "react-icons/ci";
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { IoMdHelp } from "react-icons/io";
+function KanbasNavigation() {
+const links = [
+{ label: "Account", icon: <FaRegUserCircle className="fs-2" /> },
+{ label: "Dashboard", icon: <FaTachometerAlt className="fs-2 text-danger" /> },
+{ label: "Courses", icon: <FaBook className="fs-2 text-danger" /> },
+{ label: "Calendar", icon: <FaRegCalendarAlt className="fs-2 text-danger" /> },
+{ label: "Inbox", icon: <FaInbox className="fs-2 text-danger" />},
+{ label: "History", icon: <CiClock2 className="fs-2 text-danger" />},
+{ label: "Studio", icon: <HiOutlineDesktopComputer className="fs-2 text-danger"/>},
+{ label: "Commons", icon: <FaComment className="fs-2 text-danger"/>},
+{ label: "Help", icon: <IoMdHelp className="fs-2 text-danger"/>},     
+];
+
+const { pathname } = useLocation();
+return (
+<ul className="wd-kanbas-navigation">
+<li><img src="../../images/neu.png" alt="N" className="neu-logo"/></li>
+
+
+{links.map((link, index) => (
+<li key={index} className={pathname.includes(link.label) ? "wd-active" : ""}>
+<Link to={`/Kanbas/${link.label}`}> {link.icon} {link.label} </Link>
+</li>
+))}
+
+
+</ul>
+);
+}
+export default KanbasNavigation;
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,14 +1,19 @@
 import {Link} from "react-router-dom";
 import Nav from "../Nav";
+import KanbasNavigation from "./Navigation";
 
 function Kanbas() {
-    return(
-    <div>
-    <Nav/>
-    <h1>Kanbas</h1>
+return (
+<div className="d-flex">
+<KanbasNavigation />
+    <div style={{ flexGrow: 1 }}>
+        <h1>Account</h1>
+        <h1>Dashboard</h1>
+        <h1>Courses</h1>
     </div>
-    );
-    }
+</div>
+);
+}
     export default Kanbas
     // since the Kanbas component consists of an entire application with lots of screens
     // each implemented in several files, we've decided to use an entire folder to implement
